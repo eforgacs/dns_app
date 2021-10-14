c4fe6d033daa5a658fa8d90db714a80a1549762a
Edward Forgacs <eddie.forgacs@gmail.com>
2021-10-14 17:25:56 -0400

    Update US run.py

--- US/run.py
+++ US/run.py
@@ -13,20 +13,14 @@ def welcome():
 
 @app.route('/fibonacci')
 def us_main():
-    host_name = request.args.get('hostname')
-    fs_port = request.args.get('fs_port')
-    x = request.args.get('number')
-    as_ip = request.args.get('as_ip')
-    as_port = request.args.get('as_port')
-    if not [x for x in (host_name, fs_port, x, as_ip, as_port) if x is None]:
+    if not [x for x in (request.args.get('hostname'), request.args.get('fs_port'), request.args.get('number'), request.args.get('as_ip'), request.args.get('as_port')) if x is None]:
         return Response("Bad request", status=HTTPStatus.BAD_REQUEST)
     else:
-        print(f'Success. Input information is as follows: {host_name}, {fs_port}, {x}, {as_ip}, {as_port}')
-        ip_info = {'name': host_name, 'fs_port': fs_port}
-        r = requests.get(f'https://{as_ip}:{as_port}', params=ip_info)
+        print(f"Success. Input information is as follows: {request.args.get('hostname')}, {request.args.get('fs_port')}, {request.args.get('number')}, {request.args.get('as_ip')}, {request.args.get('as_port')}")
+        ip_info = {'name': request.args.get('hostname'), 'fs_port': request.args.get('fs_port')}
+        r = requests.get(f"https://{request.args.get('as_ip')}:{request.args.get('as_port')}", params=ip_info)
         if r.status_code == HTTPStatus.NOT_FOUND:
             return f"Hostname not found. Status:{r.status_code}"
-        ip_address_fs = f'https://{r.text}:{fs_port}/fibonacci?number={x}'
-        print(ip_address_fs)
-        r = requests.get(ip_address_fs)
+        print(f"https://{r.text}:{request.args.get('fs_port')}/fibonacci?number={request.args.get('number')}")
+        r = requests.get(f"https://{r.text}:{request.args.get('fs_port')}/fibonacci?number={request.args.get('number')}")
         return r.text
